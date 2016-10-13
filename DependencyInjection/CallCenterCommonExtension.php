82ffcfda170e80d8334ed6145135cb82a436390f
Diego Rivarola <rivarolad@marketingone.ingo>
2016-10-13 01:34:12 -0300

    Setup dependency injection

--- DependencyInjection/CallCenterCommonExtension.php
+++ DependencyInjection/CallCenterCommonExtension.php
@@ -5,12 +5,36 @@ namespace CallCenter\CommonBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\Config\FileLocator;
 
 class CallCenterCommonExtension extends Extension
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yml');
 
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+
+        // Fill the array with classes when appropiate
+        $this->addClassesToCompile(array());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfiguration(array $config, ContainerBuilder $container)
+    {
+        return new Configuration($container->getParameter('kernel.debug'));
+    }
 }
