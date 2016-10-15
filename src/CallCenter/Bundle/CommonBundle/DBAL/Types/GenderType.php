39af6a09c866f3a814689c1594a81f49845dbad4
Diego Rivarola <rivarolad@marketingone.ingo>
2016-10-15 19:16:32 -0300

    Add, for real, a DBAL type

---
+++ src/CallCenter/Bundle/CommonBundle/DBAL/Types/GenderType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CallCenter\Bundle\CommonBundle\DBAL\Types;
+
+use Fresh\DoctrineEnumBundle\DBAL\Types\AbstractEnumType;
+
+final class GenderType extends AbstractEnumType
+{
+    const MALE = 'F';
+    const FEMALE = 'M';
+
+    protected static $choices = [
+        self::MALE => 'Masculino',
+        self::FEMALE => 'Femenino',
+    ];
+}
