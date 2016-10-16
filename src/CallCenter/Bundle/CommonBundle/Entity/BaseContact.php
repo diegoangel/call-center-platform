e92693c270d9ea5ef7c428a0a0cff05a65f6b4f6
Diego Rivarola <rivarolad@marketingone.ingo>
2016-10-16 02:35:05 -0300

    Change travis badge in README

--- src/CallCenter/Bundle/CommonBundle/Entity/BaseContact.php
+++ src/CallCenter/Bundle/CommonBundle/Entity/BaseContact.php
@@ -2,8 +2,12 @@
 
 namespace CallCenter\CommonBundle\Entity;
 
+use CallCenter\CommonBundle\Model\Contact;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-abstract class BaseContact
+abstract class BaseContact extends Contact
+{
+
+}
