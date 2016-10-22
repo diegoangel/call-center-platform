089694428d9e57287b671adb6355180d2cd20c9c
Diego Rivarola <rivarolad@marketingone.ingo>
2016-10-22 19:45:22 -0300

    Create entity

---
+++ src/CallCenter/Bundle/CommonBundle/Entity/BaseUser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CallCenter\CommonBundle\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use FOS\UserBundle\Entity\User as FOSUser;
+
+/**
+ * @ORM\MappedSuperclass
+ * @Gedmo\SoftDeleteable(
+ *      fieldName="deleted_at",
+ *      timeAware=false
+ * )
+ */
+abstract class BaseUser extends FOSUser
+{
+    /**
+     * @ORM\Column(
+     *      name="delete_at",
+     *      type="datetime",
+     *      nullable=true
+     * )
+     */
+    private $deletedAt;
+}
